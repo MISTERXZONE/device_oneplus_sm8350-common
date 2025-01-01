7b3ccc62297e13bb88f076ea6a5a303cf3e85c69
MISTERXZONE <porschemisterx@gmail.com>
2025-01-01 13:50:08 +0530

    Build Basic Call Recorder if exist in vendor
    Update $(call inherit-product, vendor/bcr/bcr.mk) to
    $(call inherit-product-if-exists, vendor/bcr/bcr.mk)

--- common.mk
+++ common.mk
@@ -391,3 +391,6 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
 #Sign Keys
 -include vendor/lineage-priv/keys/keys.mk
+
+#BCR
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
